ffa7fa5ef014579790665f762f08c922fa4ed423
misaac1148 <99283431+misaac1148@users.noreply.github.com>
2026-04-21 12:36:22 -0700

    Update layout.tsx to include html and body tags in default function

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,6 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
 export const metadata: Metadata = {
   metadataBase: new URL("https://mc3grp.com"),
   title: {
@@ -25,11 +28,22 @@ export const metadata: Metadata = {
   twitter: {
     card: "summary_large_image",
     title: "MC3 Group",
-    description:
-      "More Clarity. More Control. More Conversion.",
+    description: "More Clarity. More Control. More Conversion.",
     images: ["/og-image.jpg"],
   },
   icons: {
     icon: "/favicon.ico",
   },
 };
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
